17f375f3f9c061651b7501255f4f279e38cfce2b
Advox <tvadmix@gmail.com>
2024-05-08 00:43:51 +0300

    API проверка существования юзера

--- src/main/java/com/example/musicion/controller/LoginController.java
+++ src/main/java/com/example/musicion/controller/LoginController.java
@@ -1,10 +1,33 @@
 package com.example.musicion.controller;
 
+import com.example.musicion.model.auth.User;
+import com.example.musicion.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/open-api")
 public class LoginController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/checkUser")
+    public ResponseEntity<HttpStatus> findUser(@RequestParam String username) {
+        if (username.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<User> foundedUser = userRepository.findByUsername(username);
+        if (foundedUser.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
