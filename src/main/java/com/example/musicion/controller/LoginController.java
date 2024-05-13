b5f9277ef0c1ca6eb72236171831bf6e6af08424
Advox <tvadmix@gmail.com>
2024-05-13 20:55:49 +0300

    checkuser теперь post с телом

--- src/main/java/com/example/musicion/controller/LoginController.java
+++ src/main/java/com/example/musicion/controller/LoginController.java
@@ -5,10 +5,7 @@ import com.example.musicion.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,8 +16,8 @@ public class LoginController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/checkUser")
-    public ResponseEntity<HttpStatus> findUser(@RequestParam String username) {
+    @PostMapping("/checkUser")
+    public ResponseEntity<HttpStatus> findUser(@RequestBody String username) {
         if (username.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
---
+++ src/main/java/com/example/musicion/override/AppError.java
@@ -0,0 +1,2 @@
+package com.example.musicion.override;public class AppError {
+}
